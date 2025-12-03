ba3c2177504b7b36d5574c2e445ff0df88a5a493
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2025-12-03 09:52:05 +0100

    Added solution for first and second day

---
+++ elli/AOC_Day1/Program.cs
@@ -0,0 +1,116 @@
+﻿// Advent of code 2025 - Day 1 : Secret Entrance
+
+int password = 0;
+int updatedPasswordWith = 0;
+
+int dial_position = 50;
+Console.WriteLine($"- The dial starts by pointing at {dial_position}.");
+
+//string[] rotations = {"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"};
+string[] rotations = File.ReadAllLines("input.txt"); 
+
+foreach (string rotation in rotations)
+{
+    try
+    {
+        char direction = rotation[0];
+        int distance = Convert.ToInt32(rotation[1..]);
+        dial_position = RotateTheDial(dial_position, direction, distance);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error: {e}");
+    }
+}
+
+Console.WriteLine($"The password is: {password}");
+
+
+int RotateTheDial(int dial_position, char direction, int distance)
+{
+
+    if (direction == 'R')
+    {
+        dial_position += distance;
+        if (dial_position > 99)
+        {
+            int num_rotations = dial_position / 100;
+            password += num_rotations;
+            updatedPasswordWith = num_rotations;
+
+            dial_position %= 100;
+        }
+    }
+    else if (direction == 'L')
+    {
+        if (distance > dial_position && dial_position != 0)
+        {
+            int leftFromPosZero = distance - dial_position;
+            password++;
+            updatedPasswordWith++;
+  
+            if (leftFromPosZero > 99)
+            {
+                int num_rotations = leftFromPosZero / 100;
+                password += num_rotations;
+                updatedPasswordWith += num_rotations;
+                dial_position = 100 - (leftFromPosZero %= 100);
+
+                if (dial_position == 100)
+                {
+                    dial_position = 0;
+                }
+            }
+            else
+            {
+                dial_position = 100 - leftFromPosZero;
+            }
+        }
+        else if (distance > 99)
+        {
+            int num_rotations = distance / 100;
+            password += num_rotations;
+            updatedPasswordWith += num_rotations;
+            int leftAfterRotations = distance - (num_rotations * 100);
+            dial_position = 100 - leftAfterRotations;
+
+            if (dial_position == 100)
+            {
+                dial_position = 0;
+            }
+        }
+        else
+        {
+            if (dial_position == 0)
+            {
+                dial_position = 100;
+            }
+
+            dial_position -= distance;
+
+            if (dial_position == 0)
+            {
+                password++;
+                updatedPasswordWith++;
+            }
+        }
+    }
+
+    if (updatedPasswordWith > 0)
+    {
+        if (updatedPasswordWith == 1)
+        {
+            Console.WriteLine($"- The dial is rotated {direction}{distance} to point at now at {dial_position}; during this rotation, it points at 0 once.");
+        }
+        else
+        {
+            Console.WriteLine($"- The dial is rotated {direction}{distance} to point at now at {dial_position}; during this rotation, it points at 0 a number of {updatedPasswordWith} times.");
+        }
+        updatedPasswordWith = 0;
+    }
+    else
+    {
+        Console.WriteLine($"- The dial is rotated {direction}{distance} to point at now at {dial_position}.");
+    }
+    return dial_position;
+}
---
+++ elli/AOC_Day2/Program.cs
@@ -0,0 +1,105 @@
+﻿// Advent of code 2025 - Day 2 : Gift Shop
+
+double sumOfInvalidIDs = 0;
+
+//string input = "11-22, 95-115, 998-1012, 1188511880-1188511890, 222220-222224, 1698522-1698528, 446443-446449, 38593856-38593862, 565653-565659, 824824821-824824827, 2121212118-2121212124";
+string input = File.ReadAllText("input.txt"); 
+string[] ranges = input.Replace(" ", "").Split(',');
+
+foreach (string range in ranges)
+{
+    try
+    {
+        double firstID = Convert.ToDouble(range.Substring(0, range.IndexOf("-")));
+        double lastID = Convert.ToDouble(range.Substring(range.IndexOf("-") + 1));
+
+        while (firstID <= lastID)
+        {
+            string currentID = Convert.ToString(firstID);
+            int currentIDLength = currentID.Length;
+
+            // Check if all numbers are the same, if there is at least two digits.
+            string firstNumber = currentID[0].ToString();
+
+            if (currentID.Replace(firstNumber, "") == "" && currentIDLength > 1)
+            {
+                Console.WriteLine($"Invalid ID: {firstID}");
+                sumOfInvalidIDs += firstID;
+            }
+            else
+            {
+                // Check for possible factors
+                bool addedSymmetrical = false;
+                
+                for (int i = 2; i < currentIDLength; i++)
+                {
+                    // Check if divisible by any number between 2 and ID.Length-1
+                    if (currentIDLength % i == 0)
+                    {
+                        // If perfectly symmetric
+                        if (currentIDLength % 2 == 0)
+                        {
+                            int lengthOfHalf = currentIDLength / 2;
+                            double partOne = Convert.ToDouble(currentID[..lengthOfHalf]);
+                            double partTwo = Convert.ToDouble(currentID[lengthOfHalf..]);
+
+                            if (partOne == partTwo)
+                            {
+                                if (!addedSymmetrical)
+                                {
+                                    sumOfInvalidIDs += firstID;
+                                    Console.WriteLine($"Invalid ID: {firstID}");
+                                    addedSymmetrical = true;
+                                }
+                            }
+                        }
+
+                        // If not symmetric, but possible repeates
+                        if (currentIDLength % i == 0 && addedSymmetrical == false && i != 2)
+                        {
+                            List<string> possibleMatches = [];
+
+                            for (int j = 0; j < currentIDLength; j += currentIDLength / i)
+                            {
+                                string match = "";
+
+                                for (int k = 0; k < currentIDLength / i; k++)
+                                {
+                                    match += currentID[j + k];
+                                }
+
+                                possibleMatches.Add(match);
+                                match = "";
+                            }
+
+                            bool isMatch = true;
+                            for (int j = 0; j < possibleMatches.Count - 1; j++)
+                            {
+                                if (possibleMatches[j] != possibleMatches[j + 1])
+                                {
+                                    isMatch = false;
+                                    break;
+                                }
+                            }
+
+                            if (isMatch == true)
+                            {
+                                sumOfInvalidIDs += firstID;
+                                Console.WriteLine($"Invalid ID: {firstID}");
+                            } 
+                        }
+                    }
+                }
+                addedSymmetrical = false;
+            }
+
+            firstID++;
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error: {e}");
+    }
+}
+
+Console.WriteLine($"The sum of all invalid IDs are: {sumOfInvalidIDs}");

ee32c396e2a4e63bffd1c5f898b3181b43bdc949
elisabethroys <125894666+elisabethroys@users.noreply.github.com>
2026-03-24 10:36:17 +0100

    Added solution for day 4 part 2

--- elli/AOC_Day4/Program.cs
+++ elli/AOC_Day4/Program.cs
@@ -2,86 +2,120 @@
 
 double numAccessibleRolls = 0;
 
+/*string[] input =
+{
+    "..@@.@@@@.",
+    "@@@.@.@.@@",
+    "@@@@@.@.@@",
+    "@.@@@@..@.",
+    "@@.@@@@.@@",
+    ".@@@@@@@.@",
+    ".@.@.@.@@@",
+    "@.@@@.@@@@",
+    ".@@@@@@@@.",
+    "@.@.@@@.@."
+};*/
+
 string[] input = File.ReadAllLines("input.txt");
 
 int rowLength = input.Length;
 int columnLength = input[0].Length;
-Console.WriteLine($"Length {rowLength} {columnLength}");
 
-for (int i = 0; i < rowLength; i++)
+// Convert to char array
+char[,] diagramOfRolls = new char[rowLength, columnLength];
+
+for(int i = 0; i < rowLength; i++)
 {
-    int numRollsInAdjacentPositions = 0;
-
-    for (int j = 0; j < columnLength; j++)
+    for (int  j = 0; j < columnLength; j++)
     {
-        Console.WriteLine(input[i][j]);
-
-        if (input[i][j] == '.')
-        {
-            continue;
-        }
-
-        // Check the three spaces on top
-        if (i > 0)
-        {
-            if (j > 0 && input[i - 1][j - 1] == '@')
-            {
-                numRollsInAdjacentPositions++;
-                Console.WriteLine("top left: +1");
-            }
-            if (input[i - 1][j] == '@')
-            {
-                numRollsInAdjacentPositions++;
-                Console.WriteLine("top: +1");
-            }
-            if (j < columnLength - 1 && input[i - 1][j + 1] == '@')
-            {
-                numRollsInAdjacentPositions++;
-                Console.WriteLine("top right: +1");
-            }
-        }
-
-        // Check before and after
-        if (j > 0 && input[i][j - 1] == '@')
-        {
-            numRollsInAdjacentPositions++;
-            Console.WriteLine("left: +1");
-        }
-        if (j < columnLength - 1 && input[i][j + 1] == '@')
-        {
-            numRollsInAdjacentPositions++;
-            Console.WriteLine("right: +1");
-        }
-
-        // Check the three spaces on the bottom
-        if (i < rowLength - 1)
-        {
-            if (j > 0 && input[i + 1][j - 1] == '@')
-            {
-                numRollsInAdjacentPositions++;
-                Console.WriteLine("bottom left: +1");
-            }
-            if (input[i + 1][j] == '@')
-            {
-                numRollsInAdjacentPositions++;
-                Console.WriteLine("bottom: +1");
-            }
-            if (j < columnLength - 1 && input[i + 1][j + 1] == '@')
-            {
-                numRollsInAdjacentPositions++;
-                Console.WriteLine("bottom right: +1");
-            }
-        }
-
-        if (numRollsInAdjacentPositions < 4)
-        {
-            numAccessibleRolls++;
-            Console.WriteLine("Less then four!");
-        }
-        numRollsInAdjacentPositions = 0;
+        diagramOfRolls[i, j] = input[i][j];
+        //Console.Write(diagramOfRolls[i, j]);
     }
-
-    Console.WriteLine("");
+    //Console.WriteLine();
 }
 
-Console.WriteLine($"Number of accessible rolls: {numAccessibleRolls}");
+List<(int i, int j)> removeRollsAt = new List<(int i, int j)>();
+bool newRollsAdded = false;
+
+do
+{
+    foreach (var (i, j) in removeRollsAt)
+    {
+        //Console.WriteLine($"({i}, {j})");
+        diagramOfRolls[i, j] = '.';
+    }
+    newRollsAdded = false;
+
+    for (int i = 0; i < rowLength; i++)
+    {
+        int numRollsInAdjacentPositions = 0;
+
+        for (int j = 0; j < columnLength; j++)
+        {
+            //Console.WriteLine(diagramOfRolls[i, j]);
+
+            if (diagramOfRolls[i, j] == '.')
+            {
+                continue;
+            }
+
+            // Check the three spaces on top
+            if (i > 0)
+            {
+                if (j > 0 && diagramOfRolls[i - 1, j - 1] == '@')
+                {
+                    numRollsInAdjacentPositions++;
+                }
+                if (diagramOfRolls[i - 1, j] == '@')
+                {
+                    numRollsInAdjacentPositions++;
+                }
+                if (j < columnLength - 1 && diagramOfRolls[i - 1, j + 1] == '@')
+                {
+                    numRollsInAdjacentPositions++;
+                }
+            }
+
+            // Check before and after
+            if (j > 0 && diagramOfRolls[i, j - 1] == '@')
+            {
+                numRollsInAdjacentPositions++;
+            }
+            if (j < columnLength - 1 && diagramOfRolls[i, j + 1] == '@')
+            {
+                numRollsInAdjacentPositions++;
+            }
+
+            // Check the three spaces on the bottom
+            if (i < rowLength - 1)
+            {
+                if (j > 0 && diagramOfRolls[i + 1, j - 1] == '@')
+                {
+                    numRollsInAdjacentPositions++;
+                }
+                if (diagramOfRolls[i + 1, j] == '@')
+                {
+                    numRollsInAdjacentPositions++;
+                }
+                if (j < columnLength - 1 && diagramOfRolls[i + 1, j + 1] == '@')
+                {
+                    numRollsInAdjacentPositions++;
+                }
+            }
+
+            if (numRollsInAdjacentPositions < 4)
+            {
+                numAccessibleRolls++;
+                //Console.WriteLine("Less then four!");
+                removeRollsAt.Add((i, j));
+                newRollsAdded = true;
+            }
+            numRollsInAdjacentPositions = 0;
+        }
+
+        //Console.WriteLine("");
+    }
+
+    Console.WriteLine($"Number of accessible rolls: {numAccessibleRolls}");
+}
+while (newRollsAdded);
